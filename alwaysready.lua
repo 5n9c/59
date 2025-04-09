99eedeead6cba7693b4763102883e459aeb0411c
5n9c <5n9cgm@gmail.com>
2025-04-09 01:48:17 -0300

    Update alwaysready.lua

--- alwaysready.lua
+++ alwaysready.lua
@@ -1,7 +1,9 @@
-local largeSize = Vector3.new(1000, 1000, 1000)
-local originalSize = Vector3.new(70.89118957519531, 31.799999237060547, 32.072750091552734)
+local module = {}
 
-local function findReadyZone()
+module.largeSize = Vector3.new(1000, 1000, 1000)
+module.originalSize = Vector3.new(70.89118957519531, 31.799999237060547, 32.072750091552734)
+
+function module.findReadyZone()
     local workspace = game:GetService("Workspace")
     local newLobby = workspace:FindFirstChild("New Lobby")
     if not newLobby then return nil end
@@ -9,3 +11,5 @@ local function findReadyZone()
     if not readyArea then return nil end
     return readyArea:FindFirstChild("ReadyZone")
 end
+
+return module
